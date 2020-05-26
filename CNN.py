dd5a0db5d8a76d79d9d2f6e5ba4a87928d7ce758
akhil-28 <akhileshkumar2802@gmail.com>
2020-05-26 13:10:06 +0530

    first comment

---
+++ CNN.py
@@ -0,0 +1,10 @@
+
+for i in range(10):
+    accu = i+3
+    if accu < 7:
+        print("your accuracy is: ", accu)
+    else:
+        print("Your model has been successfully lauched with an accuracy of",accu)
+        break
+
+        
